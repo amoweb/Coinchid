5056ceb0f5545d14a447193a9b3db0daafb9dc54
Amaury Graillat <amaury@amoweb.fr>
2020-03-22 16:53:03 +0100

    Sets display

--- index.php
+++ index.php
@@ -31,127 +31,20 @@ html, body, .container {
 	</div>
 </nav>
 
-<h2>Equipe 1</h2>
-<a href='javascript:peuplerTas(1, "V♠");'>V♠ </a>
-&nbsp;<a href='javascript:peuplerTas(1, "9♠");'>9♠ </a>
-&nbsp;<a href='javascript:peuplerTas(1, "A♠");'>A♠ </a>
-&nbsp;<a href='javascript:peuplerTas(1, "10♠");'>10♠ </a>
-&nbsp;<a href='javascript:peuplerTas(1, "R♠");'>R♠ </a>
-&nbsp;<a href='javascript:peuplerTas(1, "D♠");'>D♠ </a>
-&nbsp;<a href='javascript:peuplerTas(1, "8♠");'>8♠ </a>
-&nbsp;<a href='javascript:peuplerTas(1, "7♠");'>7♠ </a>
-<br />
-<a href='javascript:peuplerTas(1, "V♥ ");'>V♥ </a>
-&nbsp;<a href='javascript:peuplerTas(1, "9♥");'>9♥ </a>
-&nbsp;<a href='javascript:peuplerTas(1, "A♥");'>A♥ </a>
-&nbsp;<a href='javascript:peuplerTas(1, "10♥");'>10♥ </a>
-&nbsp;<a href='javascript:peuplerTas(1, "R♥");'>R♥ </a>
-&nbsp;<a href='javascript:peuplerTas(1, "D♥");'>D♥ </a>
-&nbsp;<a href='javascript:peuplerTas(1, "8♥");'>8♥ </a>
-&nbsp;<a href='javascript:peuplerTas(1, "7♥");'>7♥ </a>
-<br />
-<a href='javascript:peuplerTas(1, "V♦");'>V♥ </a>
-&nbsp;<a href='javascript:peuplerTas(1, "9♦");'>9♦</a>
-&nbsp;<a href='javascript:peuplerTas(1, "A♦");'>A♦</a>
-&nbsp;<a href='javascript:peuplerTas(1, "10♦");'>10♦</a>
-&nbsp;<a href='javascript:peuplerTas(1, "R♦");'>R♦</a>
-&nbsp;<a href='javascript:peuplerTas(1, "D♦");'>D♦</a>
-&nbsp;<a href='javascript:peuplerTas(1, "8♦");'>8♦</a>
-&nbsp;<a href='javascript:peuplerTas(1, "7♦");'>7♦</a>
-<br />
-<a href='javascript:peuplerTas(1, "V♣ ");'>V♥ </a>
-&nbsp;<a href='javascript:peuplerTas(1, "9♣");'>9♣ </a>
-&nbsp;<a href='javascript:peuplerTas(1, "A♣");'>A♣ </a>
-&nbsp;<a href='javascript:peuplerTas(1, "10♣");'>10♣ </a>
-&nbsp;<a href='javascript:peuplerTas(1, "R♣");'>R♣ </a>
-&nbsp;<a href='javascript:peuplerTas(1, "D♣");'>D♣ </a>
-&nbsp;<a href='javascript:peuplerTas(1, "8♣");'>8♣ </a>
-&nbsp;<a href='javascript:peuplerTas(1, "7♣");'>7♣ </a>
-<br />
-
-Tas : <div id="equipe1cartes"></div>
-
-<a href="javascript:effacerTas(1);">&larr;Effacer</a>
-<br />
-
-<h2>Equipe 2</h2>
-<a href='javascript:peuplerTas(2, "V♠ ");'>V♠ </a>
-&nbsp;<a href='javascript:peuplerTas(2, "9♠");'>9♠ </a>
-&nbsp;<a href='javascript:peuplerTas(2, "A♠");'>A♠ </a>
-&nbsp;<a href='javascript:peuplerTas(2, "10♠");'>10♠ </a>
-&nbsp;<a href='javascript:peuplerTas(2, "R♠");'>R♠ </a>
-&nbsp;<a href='javascript:peuplerTas(2, "D♠");'>D♠ </a>
-&nbsp;<a href='javascript:peuplerTas(2, "8♠");'>8♠ </a>
-&nbsp;<a href='javascript:peuplerTas(2, "7♠");'>7♠ </a>
-<br />
-<a href='javascript:peuplerTas(2, "V♥ ");'>V♥ </a>
-&nbsp;<a href='javascript:peuplerTas(2, "9♥");'>9♥ </a>
-&nbsp;<a href='javascript:peuplerTas(2, "A♥");'>A♥ </a>
-&nbsp;<a href='javascript:peuplerTas(2, "10♥");'>10♥ </a>
-&nbsp;<a href='javascript:peuplerTas(2, "R♥");'>R♥ </a>
-&nbsp;<a href='javascript:peuplerTas(2, "D♥");'>D♥ </a>
-&nbsp;<a href='javascript:peuplerTas(2, "8♥");'>8♥ </a>
-&nbsp;<a href='javascript:peuplerTas(2, "7♥");'>7♥ </a>
-<br />
-<a href='javascript:peuplerTas(2, "V♦");'>V♥ </a>
-&nbsp;<a href='javascript:peuplerTas(2, "9♦");'>9♦</a>
-&nbsp;<a href='javascript:peuplerTas(2, "A♦");'>A♦</a>
-&nbsp;<a href='javascript:peuplerTas(2, "10♦");'>10♦</a>
-&nbsp;<a href='javascript:peuplerTas(2, "R♦");'>R♦</a>
-&nbsp;<a href='javascript:peuplerTas(2, "D♦");'>D♦</a>
-&nbsp;<a href='javascript:peuplerTas(2, "8♦");'>8♦</a>
-&nbsp;<a href='javascript:peuplerTas(2, "7♦");'>7♦</a>
-<br />
-<a href='javascript:peuplerTas(2, "V♣ ");'>V♥ </a>
-&nbsp;<a href='javascript:peuplerTas(2, "9♣");'>9♣ </a>
-&nbsp;<a href='javascript:peuplerTas(2, "A♣");'>A♣ </a>
-&nbsp;<a href='javascript:peuplerTas(2, "10♣");'>10♣ </a>
-&nbsp;<a href='javascript:peuplerTas(2, "R♣");'>R♣ </a>
-&nbsp;<a href='javascript:peuplerTas(2, "D♣");'>D♣ </a>
-&nbsp;<a href='javascript:peuplerTas(2, "8♣");'>8♣ </a>
-&nbsp;<a href='javascript:peuplerTas(2, "7♣");'>7♣ </a>
-<br />
-
-Tas : <div id="equipe2cartes"></div>
-
-<a href="javascript:effacerTas(2);">&larr;Effacer</a>
-<br />
+<div id="display"></div>
 
 </body>
 
 <script type="text/javascript">
 
-function distribuer() {
-	var equipe1tas = document.getElementById("equipe1cartes").innerHTML;
-	var equipe2tas = document.getElementById("equipe2cartes").innerHTML;
-	var params = "equipe1=" + equipe1tas + "&equipe2=" + equipe2tas;
-
+function update() {
 	const Http = new XMLHttpRequest();
-	const url="http://localhost/coinche/distribuer.php";
-	Http.open("POST", url, true);
-	Http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-	//Http.setRequestHeader("Content-length", params.length);
-	Http.setRequestHeader("Connection", "close");
-	Http.onload = function () {
-		// do something to response
-		console.log(this.responseText);
-	};
-
-	Http.send(params);
-}
-
-function peuplerTas(equipe, carte) {
-	document.getElementById("equipe" + equipe + "cartes").innerHTML =
-		document.getElementById("equipe" + equipe + "cartes").innerHTML + 
-		carte + ",";
-}
-
-function effacerTas(equipe, carte) {
-	var tas = document.getElementById("equipe" + equipe + "cartes").innerHTML;
-
-	var newTas = tas.substring(0, tas.substring(0, tas.length - 1).lastIndexOf(","));
-
-	document.getElementById("equipe" + equipe + "cartes").innerHTML = newTas;
+	const url='http://localhost/coinche/reader.php?game=1&player=' + noteName;
+	Http.open("GET", url);
+	Http.onreadystatechange = (e) => {
+		document.getElementById("display").innerHTML = Http.responseText;
+	}
+	Http.send();
 }
 
 </script>
